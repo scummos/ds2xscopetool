69d0b80d79f79bf3f450b9408e5e29c70e781d67
Sven Brauch <svenbrauch@googlemail.com>
2013-02-22 17:42:30 +0100

    more testing stuff in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
-#include "plotline.h"
 #include <QApplication>
 #include <QDeclarativeView>
 #include <QDeclarativeEngine>
 #include <QDeclarativeContext>
 
+#include "unistd.h"
+
+#include "plotline.h"
+#include "deviceCommunicationThread.h"
+#include "channelController.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QThread* thread = new QThread();
+    DeviceCommunicationWorker* worker = new DeviceCommunicationWorker();
+    worker->moveToThread(thread);
+    QObject::connect(thread, SIGNAL(started()), worker, SLOT(connectToDevice()));
+    thread->start();
+
+
+    AskCommandCommunicationRequest* req = new AskCommandCommunicationRequest(0, "");
+    req->commandString = "*IDN?";
+    worker->enqueue(req);
 
     qmlRegisterType<Plotline>("CustomComponents", 1, 0, "PlotLine");
 
@@ -22,8 +38,31 @@ int main(int argc, char *argv[])
     QDeclarativeItem* chartline = qobject_cast<QDeclarativeItem*>(component.create());
 
     chartline->setParentItem(rootObj);
-    view.scene()->addItem(qobject_cast<QDeclarativeItem*>(chartline));
+
+    Plotline* line = qobject_cast<Plotline*>(chartline);
+    for ( int i = 0; i < 3000; i++ ) {
+        line->data->data[i] = cos(i / 200.0) * 100;
+    }
+    line->setDataRange(QRectF(0, -150, 1000, 300));
+
     QMetaObject::invokeMethod(chartline, "setup");
+
+
+    ScopeChannelController* controller  = new ScopeChannelController(chartline, worker);
+    controller->setUpdateType(ChannelController::Periodically);
+
+
+    QDeclarativeItem* anotherLine = qobject_cast<QDeclarativeItem*>(component.create());
+    anotherLine->setParentItem(rootObj);
+    anotherLine->setProperty("color", "yellow");
+
+    line = qobject_cast<Plotline*>(anotherLine);
+    for ( int i = 0; i < 3000; i++ ) {
+        line->data->data[i] = sin(i / 200.0) * 100;
+    }
+    line->setDataRange(QRectF(0, -150, 1000, 300));
+
+    QMetaObject::invokeMethod(anotherLine, "setup");
 
     view.setGeometry(100, 100, 800, 480);
     view.show();
